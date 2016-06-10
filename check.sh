df51b543091a2cb85e838b4baf6ad867667ce8b6
catonrug <aigars.kadikis@gmail.com>
2016-06-10 19:51:06 +0300

    next

---
+++ check.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+#this code is tested un fresh 2015-11-21-raspbian-jessie-lite Raspberry Pi image
+#by default this script should be located in two subdirecotries under the home
+
+#sudo apt-get update -y && sudo apt-get upgrade -y
+#sudo apt-get install git -y
+#mkdir -p /home/pi/detect && cd /home/pi/detect
+#git clone https://github.com/catonrug/smelly-windows-updates.git && cd smelly-windows-updates && chmod +x check.sh && ./check.sh
+
+#check if script is located in /home direcotry
+pwd | grep "^/home/" > /dev/null
+if [ $? -ne 0 ]; then
+  echo script must be located in /home direcotry
+  return
+fi
+
+#it is highly recommended to place this directory in another directory
+deep=$(pwd | sed "s/\//\n/g" | grep -v "^$" | wc -l)
+if [ $deep -lt 4 ]; then
+  echo please place this script in deeper directory
+  return
+fi
+
+#set application name based on directory name
+#this will be used for future temp directory, database name, google upload config, archiving
+appname=$(pwd | sed "s/^.*\///g")
+
+#set temp directory in variable based on application name
+tmp=$(echo ../tmp/$appname)
+
+#create temp directory
+if [ ! -d "$tmp" ]; then
+  mkdir -p "$tmp"
+fi
+
+#check if database directory has prepared 
+if [ ! -d "../db" ]; then
+  mkdir -p "../db"
+fi
+
+#set database variable
+db=$(echo ../db/$appname.db)
+
+#if database file do not exist then create one
+if [ ! -f "$db" ]; then
+  touch "$db"
+fi
+
+#check if google drive config directory has been made
+#if the config file exists then use it to upload file in google drive
+#if no config file is in the directory there no upload will happen
+if [ ! -d "../gd" ]; then
+  mkdir -p "../gd"
+fi
+
+if [ -f ~/uploader_credentials.txt ]; then
+sed "s/folder = test/folder = `echo $appname`/" ../uploader.cfg > ../gd/$appname.cfg
+else
+echo google upload will not be used cause ~/uploader_credentials.txt do not exist
+fi
+
+
+updateidlist=$(echo "a7e57c92-62fa-4844-948e-31187fd48e81" | sed '$alast line')
+
+printf %s "$updateidlist" | while IFS= read -r link
+do {
+
+#use spider mode to look what content are exactly are under the link
+python html-downloader.py "http://catalog.update.microsoft.com/v7/site/ScopedViewInline.aspx?updateid=$link" test.html
+
+
+} done
+
+
+#clean and remove whole temp direcotry
+rm $tmp -rf > /dev/null
